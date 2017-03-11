a2b3a75be783c2568422421591d9aedfbbe63b9a
Jenny Lian <jennylian100@gmail.com>
2017-03-11 11:35:59 -0800

    Fabian showed us how to do this in the console.

--- help-queue/src/App.js
+++ help-queue/src/App.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var firebase = require("firebase");
+var config = {
+    apiKey: "AIzaSyAA4QmoUVWffWgPAXFAm4JtJL25LkQMV8Y",
+    authDomain: "helpqueue-b72fd.firebaseapp.com",
+    databaseURL: "https://helpqueue-b72fd.firebaseio.com",
+    storageBucket: "helpqueue-b72fd.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+window.database = firebase.database();
+
 class App extends Component {
+
+
+    //constructor(){
+    //    super();
+    //    this.set = {
+    //      now_serving: 1
+    //    };
+    //}
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
