0c321ee9f4169af4feb8a7ad3e4ef151c20ee2b6
solohikertoo <solohikertoo@gmail.com>
2017-03-11 15:15:40 -0800

    added timer and some css

--- help-queue/src/App.js
+++ help-queue/src/App.js
@@ -27,14 +27,18 @@ class App extends Component {
         super();
         this.state ={
           now_serving: "gord",
-          name_queue:["jenny","victoria","cici"]
+          name_queue:["jenny","victoria","cici"],
         };
     }
     
     render() {
         return (
            <div>
-               <NowServing studentName = {this.state.now_serving}/>
+               <h1>HELP Q</h1>
+               <div className = "nowserving">
+                  <NowServing studentName = {this.state.now_serving}/>
+                  <Timer duration = "300"/>
+               </div>
                <NameQueue studentQueue = {this.state.name_queue}/>
            </div>
         );
@@ -54,7 +58,43 @@ class NowServing extends Component {
 }
 
 class Timer extends Component {
+     constructor(props) {
+         super(props)
+         var duration = this.props.duration;
+         var timer = parseInt(duration,10);
+         this.state = {
+             timer: timer,
+             timeString: ''
+         };
+         
+         setInterval(() => {
+             --timer;
+             if (timer <0) {
+                 timer = parseInt(duration,10);
+             }
+             var minutes = Math.floor(timer / 60);
+             var seconds = timer % 60;
+             var timeString = '';
+             minutes = minutes < 10 ? "0" + minutes.toString() : minutes.toString();
+             seconds = seconds < 10 ? "0" + seconds.toString() : seconds.toString();
+             timeString = minutes + ":" + seconds;
+            this.setState({timeString: timeString})
+           
+             
+         }, 1000)
+     }
      render() {
+         //var time = "1:20";
+        
+         const timeString = this.state.timeString;
+
+        
+         
+         return(
+             <div>
+                 <h3>Time left: {timeString}</h3>
+             </div>
+         );
      }
 }
 
