1416918b7013a7d06f01c01114021caac1ddb175
Jenny Lian <jennylian100@gmail.com>
2017-03-11 12:12:19 -0800

    added the now serving state

--- help-queue/src/App.js
+++ help-queue/src/App.js
@@ -3,19 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    //Set state. 1-st thing
+    constructor() {
+        super();
+        this.state ={
+          now_serving: 1,
+            queue:[]
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <h1>{this.state.now_serving}</h1>
+
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
